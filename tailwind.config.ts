aee32df59f9eda55946f53cc637cd37efc7660f3
Taha Amiri <amiri.taha.98@gmail.com>
2024-01-19 00:51:23 -0800

    [INIT] Project initial

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,11 @@
 import type { Config } from "tailwindcss";
+const colors = require('tailwindcss/colors')
 
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./utils/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
@@ -14,6 +16,22 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    colors:{
+      ...colors,
+      yellow: '#F9B800',
+      blue: '#0052B4',
+      purple: '#9747FF',
+      red: '#D80027',
+      dark: '#262626',
+      grey: '#787878',
+      light: {
+        0: '#FFF1C9',
+        1: '#FEEDBC',
+        2: '#FFE18B',
+        3: '#D3B970',
+        4: '#FFBB45',
+      }
+    }
   },
   plugins: [],
 };
---
+++ utils/shared/Header.tsx
@@ -0,0 +1,21 @@
+import Logo from '../../assets/logo.png'
+import Flag from '../../assets/flag.png'
+import Image from 'next/image'
+
+const Header = () => {
+    return (
+        <div className='flex items-center justify-between py-3 px-[22px] bg-white rounded-[3750px]'>
+            <Image
+                src={Logo}
+                alt='logo'
+                className='w-[95px] h-auto'
+            />
+            <Image
+                src={Flag}
+                alt='flag'
+            />
+        </div>
+    )
+}
+
+export default Header
