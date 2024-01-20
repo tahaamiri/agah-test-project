6dc84c805dfb40a89dcf1ef8256f6e945870a07c
Taha Amiri <amiri.taha.98@gmail.com>
2024-01-20 11:36:35 -0800

    [ADD] Section two

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
-import Header from "@/utils/shared/Header";
-import Image1 from '@/assets/DrawKit Vector Illustration-1.svg';
 import Image from "next/image";
+import Header from "@/utils/shared/Header";
+import Section1Img from '@/assets/DrawKit Vector Illustration-1.svg';
+import Section2Img from '@/assets/section2.svg';
 import ArrowIcon from "@/assets/icons/ArrowIcon";
 import TickIcon from "@/assets/icons/TickIcon";
 
@@ -13,8 +14,8 @@ export default function Home() {
             <Header />
             <section className="flex flex-col md:flex-row-reverse md:mt-[115px] pb-10 px-5 md:px-10 lg:px-[160px]">
                 <Image
-                    src={Image1}
-                    alt="Image title"
+                    src={Section1Img}
+                    alt="Section one image"
                     className="w-full max-w-sm lg:max-w-md mx-auto"
                 />
                 <div className="-mt-5 flex-1">
@@ -41,6 +42,33 @@ export default function Home() {
                     </button>
                 </div>
             </section>
+            <section className="parent bg-yellow py-24 mt-16 w-full" >
+                <div className="child flex flex-col md:flex-row-reverse md:mt-[115px] pb-10 px-5 md:px-10 lg:px-[160px]">
+                    <Image
+                        src={Section2Img}
+                        alt="Section two image"
+                        className="w-full max-w-sm lg:max-w-md mx-auto"
+                    />
+                    <div>
+                        <h1 className="text-4xl font-bold text-left">
+                            Phasellus a vitae
+                            iaculis magna
+                            eleifend pulvinar
+                            velit odio.
+                        </h1>
+                        <p className="text-xl font-semibold text-left underline mt-5">
+                            Vulputate et vulputate suspendisse natoque!
+                        </p>
+                        <p className="text-xl font-normal text-left mt-3">
+                            Euismod magna id purus eget nunc ligula suspendisse dui netus. Condimentum blandit rutrum at mauris enim pulvinar duis etiam duis vulputate et vulputate suspendisse natoque id tellus consectetur pulvinar et.
+                        </p>
+                        <button className="flex items-center justify-between w-full max-w-[350px] outline-none py-4 px-[38px] text-white text-[18.46px] font-bold bg-dark rounded-[3846.15px] mt-10">
+                            Lorem Ipsum
+                            <ArrowIcon className="fill-white" />
+                        </button>
+                    </div>
+                </div>
+            </section >
         </main>
     );
 }
